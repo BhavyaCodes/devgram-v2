54810fc92a418405228b0b0456329d56499334da
BhavyaCodes <xbhavyatomar@gmail.com>
2023-05-03 01:03:09 +0530

    setup basic project

---
+++ src/pages/index.tsx
@@ -0,0 +1,90 @@
+import { trpc } from '../utils/trpc';
+import { NextPageWithLayout } from './_app';
+import { inferProcedureInput } from '@trpc/server';
+import Link from 'next/link';
+import { Fragment } from 'react';
+import type { AppRouter } from '~/server/routers/_app';
+
+const IndexPage: NextPageWithLayout = () => {
+  const utils = trpc.useContext();
+
+  const myString = trpc.post.sayHi.useQuery('dfgrtrhrth');
+  console.log(myString.data);
+  // const postsQuery = trpc.post.list.useInfiniteQuery(
+  //   {
+  //     limit: 5,
+  //   },
+  //   {
+  //     getPreviousPageParam(lastPage) {
+  //       return lastPage.nextCursor;
+  //     },
+  //   },
+  // );
+
+  // const addPost = trpc.post.add.useMutation({
+  //   async onSuccess() {
+  //     // refetches posts after a post is added
+  //     await utils.post.list.invalidate();
+  //   },
+  // });
+
+  // prefetch all posts for instant navigation
+  // useEffect(() => {
+  //   const allPosts = postsQuery.data?.pages.flatMap((page) => page.items) ?? [];
+  //   for (const { id } of allPosts) {
+  //     void utils.post.byId.prefetch({ id });
+  //   }
+  // }, [postsQuery.data, utils]);
+
+  return (
+    <>
+      <h1>Welcome to your tRPC starter!</h1>
+      <p>
+        If you get stuck, check <a href="https://trpc.io">the docs</a>, write a
+        message in our <a href="https://trpc.io/discord">Discord-channel</a>, or
+        write a message in{' '}
+        <a href="https://github.com/trpc/trpc/discussions">
+          GitHub Discussions
+        </a>
+        .
+      </p>
+
+      <h2>
+        Latest Posts
+        {/* {postsQuery.status === 'loading' && '(loading)'} */}
+      </h2>
+
+      <hr />
+
+      <h3>Add a Post</h3>
+    </>
+  );
+};
+
+export default IndexPage;
+
+/**
+ * If you want to statically render this page
+ * - Export `appRouter` & `createContext` from [trpc].ts
+ * - Make the `opts` object optional on `createContext()`
+ *
+ * @link https://trpc.io/docs/ssg
+ */
+// export const getStaticProps = async (
+//   context: GetStaticPropsContext<{ filter: string }>,
+// ) => {
+//   const ssg = createServerSideHelpers({
+//     router: appRouter,
+//     ctx: await createContext(),
+//   });
+//
+//   await ssg.post.all.fetch();
+//
+//   return {
+//     props: {
+//       trpcState: ssg.dehydrate(),
+//       filter: context.params?.filter ?? 'all',
+//     },
+//     revalidate: 1,
+//   };
+// };
