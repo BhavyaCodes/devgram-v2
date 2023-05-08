ab401c69c6fa4ee0a356a09c00fbcd351d99de86
BhavyaCodes <xbhavyatomar@gmail.com>
2023-05-08 17:29:42 +0530

    create comment model

---
+++ src/server/models/Comment.ts
@@ -0,0 +1,25 @@
+import { ObjectId } from 'mongodb';
+import mongoose, { Model, Schema, model } from 'mongoose';
+
+export interface IComment {
+  _id: ObjectId;
+  userId: ObjectId;
+  postId: ObjectId;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const commentSchema = new Schema<IComment>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    postId: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+    content: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+const Comment = model<IComment>('Comment', commentSchema);
+export default Comment;
+// export default (mongoose.models?.Comment as Model<IComment>) ||
+//   model<IComment>('Comment', commentSchema);
---
+++ src/server/routers/comment.ts
@@ -0,0 +1,59 @@
+/**
+ *
+ * This is an example router, you can delete this file and then update `../pages/api/trpc/[trpc].tsx`
+ */
+import { ObjectId } from 'mongodb';
+import { authOnlyProcedure, currentSessionProcedure } from '../middleware';
+import Post, { IPost } from '../models/Post';
+import { router, publicProcedure } from '../trpc';
+// import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+import { FilterQuery, PipelineStage, Types } from 'mongoose';
+import Like from '../models/Like';
+import { TRPCError } from '@trpc/server';
+import Comment from '../models/Comment';
+/**
+ *
+ * Default selector for Post.
+ * It's important to always explicitly say which fields you want to return in order to not leak extra information
+ * @see https://github.com/prisma/prisma/issues/9353
+ */
+// const defaultPostSelect = Prisma.validator<Prisma.PostSelect>()({
+//   id: true,
+//   title: true,
+//   text: true,
+//   createdAt: true,
+//   updatedAt: true,
+// });
+
+export const commentRouter = router({
+  addComment: authOnlyProcedure
+    .input(
+      z.object({
+        postId: z.string(),
+        content: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { content, postId } = input;
+      const user = ctx.session.userId;
+
+      const count = await Post.countDocuments({ _id: postId });
+
+      if (!count) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'Post does not exist',
+        });
+      }
+
+      const newComment = new Comment({
+        content,
+        postId,
+        userId: user._id,
+      });
+
+      await newComment.save();
+      return newComment;
+    }),
+});
