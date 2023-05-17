fbe6432c62f48907063c498fa616e683972c693c
BhavyaCodes <xbhavyatomar@gmail.com>
2023-05-17 17:56:12 +0530

    remove scroll to top

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -16,43 +16,43 @@ import logoTextBlack from '~/assets/logo-text-dark.svg';
 
 type DefaultLayoutProps = { children: ReactNode };
 
-function ScrollTop({ children }: { children: ReactNode }) {
-  // const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    // target: window ? window : undefined,
-    // target:
-    disableHysteresis: true,
-    threshold: 100,
-  });
+// function ScrollTop({ children }: { children: ReactNode }) {
+//   // const { children, window } = props;
+//   // Note that you normally won't need to set the window ref as useScrollTrigger
+//   // will default to window.
+//   // This is only being set here because the demo is in an iframe.
+//   // const trigger = useScrollTrigger({
+//   //   // target: window ? window : undefined,
+//   //   // target:
+//   //   disableHysteresis: true,
+//   //   threshold: 100,
+//   // });
 
-  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    console.log('click');
-    // const anchor = (
-    //   (event.target as HTMLDivElement).ownerDocument || document
-    // ).querySelector('#back-to-top-anchor');
+//   const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+//     console.log('click');
+//     // const anchor = (
+//     //   (event.target as HTMLDivElement).ownerDocument || document
+//     // ).querySelector('#back-to-top-anchor');
 
-    // if (anchor) {
-    //   anchor.scrollIntoView({
-    //     block: 'center',
-    //   });
-    // }
-  };
+//     // if (anchor) {
+//     //   anchor.scrollIntoView({
+//     //     block: 'center',
+//     //   });
+//     // }
+//   };
 
-  return (
-    <Fade in={trigger}>
-      <Box
-        onClick={handleClick}
-        role="presentation"
-        sx={{ position: 'fixed', bottom: 16, right: 16 }}
-      >
-        {children}
-      </Box>
-    </Fade>
-  );
-}
+//   return (
+//     <Fade in={true}>
+//       <Box
+//         onClick={handleClick}
+//         role="presentation"
+//         sx={{ position: 'fixed', bottom: 16, right: 16 }}
+//       >
+//         {children}
+//       </Box>
+//     </Fade>
+//   );
+// }
 
 export const DefaultLayout = ({ children }: DefaultLayoutProps) => {
   return (
@@ -78,11 +78,11 @@ export const DefaultLayout = ({ children }: DefaultLayoutProps) => {
       </AppBar>
       <Toolbar id="back-to-top-anchor" />
       <main>{children}</main>
-      <ScrollTop>
+      {/* <ScrollTop>
         <Fab size="small" aria-label="scroll back to top">
           <KeyboardArrowUp />
         </Fab>
-      </ScrollTop>
+      </ScrollTop> */}
     </>
   );
 };
