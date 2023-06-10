7b6d8dffa776af5080baa56bcf396133bb3aa1de
BhavyaCodes <xbhavyatomar@gmail.com>
2023-06-10 17:29:00 +0530

    update getPublicProfile query after editProfile mutation

--- src/components/EditProfileModal/index.tsx
+++ src/components/EditProfileModal/index.tsx
@@ -1,13 +1,17 @@
 import {
+  Box,
   Button,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
+  TextField,
+  Typography,
   useMediaQuery,
   useTheme,
 } from '@mui/material';
-import React from 'react';
+import React, { FormEventHandler, useEffect, useState } from 'react';
+import { trpc } from '~/utils/trpc';
 
 interface EditProfileModalProps {
   open: boolean;
@@ -15,8 +19,56 @@ interface EditProfileModalProps {
 }
 
 const EditProfileModal = ({ open, handleClose }: EditProfileModalProps) => {
+  const utils = trpc.useContext();
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down('md'));
+
+  const [name, setName] = useState('');
+  const [bio, setBio] = useState('');
+  const getUser = trpc.user.getUser.useQuery();
+
+  const editUser = trpc.user.editProfile.useMutation({
+    onSuccess: (data) => {
+      console.log('onSucess');
+      utils.user.getPublicProfile.setData(
+        { profileId: data._id.toString() },
+        (oldData) => {
+          if (!oldData) {
+            return undefined;
+          }
+          return { ...oldData, ...data };
+        },
+      );
+    },
+  });
+
+  useEffect(() => {
+    if (getUser.data?.name) {
+      setName(getUser.data.name);
+    }
+  }, [getUser.data?.name]);
+
+  useEffect(() => {
+    if (getUser.data?.bio) {
+      setBio(getUser.data.bio);
+    }
+  }, [getUser.data?.bio]);
+
+  const handleSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+    editUser.mutateAsync({ name, bio }).then(() => {
+      handleClose();
+    });
+  };
+
+  if (getUser.isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (!getUser.data) {
+    return null;
+  }
+
   return (
     <Dialog
       fullScreen={matches}
@@ -27,13 +79,37 @@ const EditProfileModal = ({ open, handleClose }: EditProfileModalProps) => {
       aria-describedby="alert-dialog-description"
     >
       <DialogTitle id="alert-dialog-title">Edit Profile</DialogTitle>
-      <DialogContent></DialogContent>
-      <DialogActions>
-        <Button color="info" onClick={handleClose}>
-          Cancel
-        </Button>
-        <Button color="primary">Save</Button>
-      </DialogActions>
+      <Box component="form" onSubmit={handleSubmit}>
+        <DialogContent>
+          <TextField
+            fullWidth
+            label="Name"
+            value={name || ''}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Typography component="p" ml="auto" textAlign="right" gutterBottom>
+            {`${name.length}/50`}
+          </Typography>
+
+          <TextField
+            fullWidth
+            label="Bio"
+            value={bio || ''}
+            onChange={(e) => setBio(e.target.value)}
+          />
+          <Typography component="p" ml="auto" textAlign="right" gutterBottom>
+            {`${bio.length}/200`}
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button color="info" type="button" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button color="primary" type="submit">
+            Save
+          </Button>
+        </DialogActions>
+      </Box>
     </Dialog>
   );
 };
