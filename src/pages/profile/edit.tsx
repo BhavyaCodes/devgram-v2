67ae48052bdc39679c6e2c60cc9f1a769c02f319
BhavyaCodes <xbhavyatomar@gmail.com>
2023-06-10 11:52:00 +0530

    update name from frontend

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -1,5 +1,47 @@
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { FormEventHandler, useEffect, useState } from 'react';
+import { trpc } from '~/utils/trpc';
+
 const EditProfile = () => {
-  return <div>EditProfile</div>;
+  const [name, setName] = useState('');
+  const getUser = trpc.user.getUser.useQuery();
+  const editUser = trpc.user.editProfile.useMutation();
+
+  useEffect(() => {
+    if (getUser.data?.name) {
+      setName(getUser.data.name);
+    }
+  }, [getUser.data?.name]);
+
+  const handleSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+    editUser.mutate({ name });
+  };
+
+  if (getUser.isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (!getUser.data) {
+    return null;
+  }
+
+  return (
+    <Box>
+      <Typography variant="h2" component="h1">
+        Edit Profile
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit}>
+        <TextField
+          label="name"
+          value={name || ''}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <Button type="submit">Save</Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default EditProfile;
